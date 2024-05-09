29fa17d2cabd223361a9a1165eb01bded6e0f32d
鸭鸭「カモ」 <Yzcbs123@163.com>
2024-05-10 00:04:35 +0800

    测试用控制台输出

--- zh_cn/js/PersonalCount.js
+++ zh_cn/js/PersonalCount.js
@@ -5,6 +5,7 @@ if (!pgcount) {
 }
 pgcount++;
 localStorage.setItem("page_view_pgcount", pgcount);
+console.log(`[TEST]个人访问量：${pgcount}`)
 
 document.addEventListener("DOMContentLoaded", function() {
     // 获取已有的总访问量
@@ -12,7 +13,8 @@ document.addEventListener("DOMContentLoaded", function() {
     var totalViewsText = totalViewsElement.innerText;
     // 使用正则表达式提取数字
     var totalViews = parseInt(totalViewsText.match(/\d+/)[0]);
-
+    
+    console.log(`[TEST]总访问量：${totalViews}`)
     // 计算非个人访问量
     var NoPersonal = totalViews - pgcount;
     console.log("[DEV:INFO]非自己的访问量：" + NoPersonal);
