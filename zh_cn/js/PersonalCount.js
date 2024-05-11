d9614d24bde775b1c14603beb56c4a9377342882
鸭鸭「カモ」 <Yzcbs123@163.com>
2024-05-11 09:57:11 +0800

    简化JS代码

--- zh_cn/js/PersonalCount.js
+++ zh_cn/js/PersonalCount.js
@@ -9,29 +9,17 @@ if (pgcount == 65){
     console.warn("[WARN] 个人访问量为空值！")
 }
 
-window.onload = function() {
-    // 定义MutationObserver回调函数
-    var observer = new MutationObserver(function(mutationsList, observer) {
-        for (var mutation of mutationsList) {
-            if (mutation.type === 'childList') {
-                // 获取已有的总访问量
-                var totalViewsElement = document.getElementById("finicount_views");
-                var totalViews = totalViewsElement.innerText;
-                
-                if (!isNaN(totalViews)) {
-                    // 计算非个人访问量
-                    var NoPersonal = parseInt(totalViews) - pgcount;
-                    if (isNaN(NoPersonal)) {
-                        console.warn("[WARN] 非个人访问量为空值，请查看是否存在其他警告！")
-                    }
-                    console.log("[DEV:INFO] 非自己的访问量：" + NoPersonal);
-                    // 停止监听
-                    observer.disconnect();
-                }
-            }
+(function() {
+    // 获取已有的总访问量
+    var totalViewsElement = document.getElementById("finicount_views");
+    var totalViews = totalViewsElement.innerText;
+
+    if (!isNaN(totalViews)) {
+        // 计算非个人访问量
+        var NoPersonal = parseInt(totalViews) - pgcount;
+        if (isNaN(NoPersonal)) {
+            console.warn("[WARN] 非个人访问量为空值，请查看是否存在其他警告！")
         }
-    });
-    
-    // 监听<span id="finicount_views"></span>元素内容的变化
-    observer.observe(document.getElementById('finicount_views'), { childList: true });
-};
+        console.log("[DEV:INFO] 非自己的访问量：" + NoPersonal);
+    }
+})();
