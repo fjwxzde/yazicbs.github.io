fe76cff708861db450e8fdbdf227185bafb3da50
鸭鸭「カモ」 <Yzcbs123@163.com>
2024-06-15 17:46:21 +0800

    perf:优化Guess Number JS

--- zh_cn/js/games/guess_number.js
+++ zh_cn/js/games/guess_number.js
@@ -1,11 +1,17 @@
 // 全局变量，用于存储随机数
 var Guess_number_random_number;
 Guess_number_random_number = "No_init";
+var guess_max = NaN;
+var guess_min = NaN;
+Guess_number_init(0, 1000)
 console.log("[INFO] 试试 Guess_number(n) 吗？")
 
 // 创建一个函数，生成指定范围内的随机整数
 function Guess_number_init(min, max) {
+  guess_max = max;
+  guess_min = min;
   Guess_number_random_number = Math.floor(Math.random() * (max - min + 1)) + min;
+  return `已成功初始化猜数字游戏 (Max:${guess_max} | Min:${guess_min})`
 }
 
 // 游戏函数 - 猜数字
@@ -21,7 +27,10 @@ function Guess_number(n) {
   } else if (Guess_number_random_number === "No_init") {
     console.warn("[Game\\Guess Number] 请先使用 Guess_number_init(最小数, 最大数) 来初始化游戏！");
   } else {
-    Guess_number_random_number = "No_init";
+    if (!Number.isNaN(guess_max) && !Number.isNaN(guess_min)) {
+      Guess_number_init(guess_max, guess_min);
+      return '恭喜，您猜对了！\n已自动初始化下一局游戏,如需修改范围请重新使用 Guess_number_init(最小数, 最大数) 初始化';
+    }
     return '恭喜，您猜对了！';
   }
 }
