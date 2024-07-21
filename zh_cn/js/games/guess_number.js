62f903d701f7a0996217618de0ecfe7ec47658a2
鸭鸭「カモ」 <Yzcbs123@163.com>
2024-07-21 16:31:57 +0800

    fix: Fix DuckDuckStudio/Chinese_git#24

--- zh_cn/js/games/guess_number.js
+++ zh_cn/js/games/guess_number.js
@@ -1,4 +1,4 @@
-// 全局变量，用于存储随机数
+// 全局变量,用于存储随机数
 var Guess_number_random_number;
 Guess_number_random_number = "No_init";
 var guess_max = NaN;
@@ -6,7 +6,7 @@ var guess_min = NaN;
 Guess_number_init(0, 1000)
 console.log("[INFO] 试试 Guess_number(n) 吗？[已自动初始化(Max:1000 | Min:0)]")
 
-// 创建一个函数，生成指定范围内的随机整数
+// 创建一个函数,生成指定范围内的随机整数
 function Guess_number_init(min, max) {
   guess_max = max;
   guess_min = min;
@@ -21,16 +21,16 @@ function Guess_number(n) {
 
   // 判断猜测结果
   if (n < numberToGuess) {
-    return '太小了，请继续猜测！';
+    return '太小了,再猜看看?';
   } else if (n > numberToGuess) {
-    return '太大了，请继续猜测！';
+    return '太大了,再猜看看?';
   } else if (Guess_number_random_number === "No_init") {
     console.warn("[Game\\Guess Number] 请先使用 Guess_number_init(最小数, 最大数) 来初始化游戏！");
   } else {
     if (!Number.isNaN(guess_max) && !Number.isNaN(guess_min)) {
       Guess_number_init(guess_max, guess_min);
-      return '恭喜，您猜对了！\n已自动初始化下一局游戏,如需修改范围请重新使用 Guess_number_init(最小数, 最大数) 初始化';
+      return '恭喜,您猜对了！\n已自动初始化下一局游戏,如需修改范围请重新使用 Guess_number_init(最小数, 最大数) 初始化';
     }
-    return '恭喜，您猜对了！';
+    return '恭喜,您猜对了！';
   }
 }
--- zh_cn/js/games/simplified/guess_number.js
+++ zh_cn/js/games/simplified/guess_number.js
@@ -1,4 +1,4 @@
-// 全局变量，用于存储随机数
+// 全局变量,用于存储随机数
 var gm_random_number;
 gm_random_number = "No_init";
 var guess_max = NaN;
@@ -7,7 +7,7 @@ gm_init(0, 1000)
 console.log("[INFO] 试试 gm(n) 吗？[已自动初始化(Max:1000 | Min:0)]")
 console.warn("加载的游戏为简化版")
 
-// 创建一个函数，生成指定范围内的随机整数
+// 创建一个函数,生成指定范围内的随机整数
 function gm_init(min, max) {
   guess_max = max;
   guess_min = min;
@@ -22,16 +22,16 @@ function gm(n) {
 
   // 判断猜测结果
   if (n < numberToGuess) {
-    return '太小了，请继续猜测！';
+    return '太小了,再猜看看?';
   } else if (n > numberToGuess) {
-    return '太大了，请继续猜测！';
+    return '太大了,再猜看看?';
   } else if (gm_random_number === "No_init") {
     console.warn("[Game\\Guess Number] 请先使用 gm_init(最小数, 最大数) 来初始化游戏！");
   } else {
     if (!Number.isNaN(guess_max) && !Number.isNaN(guess_min)) {
       gm_init(guess_max, guess_min);
-      return '恭喜，您猜对了！\n已自动初始化下一局游戏,如需修改范围请重新使用 gm_init(最小数, 最大数) 初始化';
+      return '恭喜,您猜对了！\n已自动初始化下一局游戏,如需修改范围请重新使用 gm_init(最小数, 最大数) 初始化';
     }
-    return '恭喜，您猜对了！';
+    return '恭喜,您猜对了！';
   }
 }
