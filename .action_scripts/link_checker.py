79b4051606bb3f349ba1b72670d15f505c43ca2a
鸭鸭「カモ」 <Yzcbs123@163.com>
2024-09-23 03:21:12 +0800

    喵~喵! (大声叫) (爪子爬爬) (吸吸猫条) (跳到腿上) (缩成一团) 喵~❤

--- .action_scripts/link_checker.py
+++ .action_scripts/link_checker.py
@@ -8,17 +8,7 @@ IGNORE_FOLDERS = ['music_games'] # 忽略的文件夹
 IGNORE_FILES = ['Duck Parkour.html'] # 忽略的文件
 IGNORE_URLS = [
     # 常时
-    'https://space.bilibili.com/2054654702/',
-    'https://duckduckstudio.github.io/yazicbs.github.io/Tools/Fufu_Tools/wiki/error/',
     'https://duckduckstudio.github.io/yazicbs.github.io/Interesting/sounds/*.mp3',
-    'https://duckduckstudio.github.io/yazicbs.github.io/music_games/photos/show.png',
-    'https://duckduckstudio.github.io/yazicbs.github.io/music_games/photos/Arcaea/[SongName]-SCORE-EXp-FR.png',
-    'https://duckduckstudio.github.io/yazicbs.github.io/music_games/photos/GenshinImpact/[SongName]-SCORE-LEVER.png',
-    'https://duckduckstudio.github.io/yazicbs.github.io/music_games/photos/MuseDash/[SongName]-SCORE-LEVER.png',
-    'https://duckduckstudio.github.io/yazicbs.github.io/music_games/photos/Orzmic/[SongName]-SCORE-LEVER.png',
-    'https://duckduckstudio.github.io/yazicbs.github.io/music_games/photos/Other/[SongName]-SCORE-LEVER.png',
-    'https://duckduckstudio.github.io/yazicbs.github.io/music_games/photos/Phigros/[SongName]-SCORE-LEVER.png',
-    'https://duckduckstudio.github.io/yazicbs.github.io/music_games/photos/Project_Sekai/[SongName]-SCORE-LEVER.png'
 ] # 忽略的链接
 
 # 更新后的链接正则表达式
